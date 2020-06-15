3758e55a9a99a290d8275d8578875b0145721719
Rizki Hanif <qikcup@gmail.com>
2020-06-15 20:44:46 +0700

    rizki_hanif

---
+++ README.md
@@ -0,0 +1,31 @@
+yii2-thumbnail
+==============
+Widget Thumbnail bootstrap for Yii Framework 2
+
+Installation
+------------
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+php composer.phar require --prefer-dist rizkihanif/yii2-thumbnail "*"
+```
+
+or add
+
+```
+"rizkihanif/yii2-thumbnail": "*"
+```
+
+to the require section of your `composer.json` file.
+
+
+Usage
+-----
+
+Once the extension is installed, simply use it in your code by  :
+
+```php
+<?= \rizkihanif\bs\AutoloadExample::widget(); ?>```
---
+++ Thumbnail.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace rizkihanif\bs;
+
+use \yii\bootstrap\Widget;
+
+class Thumbnail extends Widget
+{
+    /**
+     * @var string $title
+     */
+    public $title = 'yii2-thumbnail';
+
+    /**
+     * @var string $description
+     */
+    public $description = 'Widget Thumbnail bootstrap for Yii Framework 2';
+
+    /**
+     * @var array $imgOptions
+     */
+    public $imgSource = 'http://placehold.it/240x200';
+
+    /**
+     * @var string $template
+     */
+    public $template = <<<HTML
+    <div class="thumbnail">
+        <img src="{imgSource}">
+        <div class="caption">
+            <h3>{title}</h3>
+            <p>{description}</p>
+        </div>
+    </div>
+    HTML;
+
+
+
+    public function init()
+    {
+        parent::init();
+        echo strtr(
+            $this->template,
+            [
+                '{imgSource}' => $this->imgSource,
+                '{title}' => $this->title,
+                '{description}' => $this->description,
+            ]
+        );        
+    }
+}
